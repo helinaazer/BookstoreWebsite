31d0e4448900cfa6e3f9b9dccdfc4d7fd282952d
Helina Azer <hazer@uw.edu>
2024-08-26 09:53:52 -0700

    Implementing Product Details Component

--- clientapp/src/App.js
+++ clientapp/src/App.js
@@ -2,7 +2,8 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Contact from "./Pages/Contact";
-import Cart from "./Pages/Cart"
+import Cart from "./Pages/Cart";
+import Product from "./Pages/Product";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/cart" element={<Cart />} />
-        
+        <Route path="/product/:id" element={<Product />} />{" "}
+        {/* Dynamic route */}
       </Routes>
     </Router>
   );
--- clientapp/src/Pages/Home.js
+++ clientapp/src/Pages/Home.js
@@ -1,28 +1,28 @@
 import React from "react";
 import NavBar from "../Components/NavBar";
-import ItemCard from '../Components/ItemCard';
-import Grid from '@mui/material/Grid';
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box'; // Import Box from Material-UI
-import './Home.css'; // Import the CSS file
+import ItemCard from "../Components/ItemCard";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box"; // Import Box from Material-UI
+import "./Home.css"; // Import the CSS file
 
 const Home = () => {
   return (
-    <div style={{ backgroundColor: "#e7decb"}}>
+    <div style={{ backgroundColor: "#e7decb" }}>
       <NavBar />
       <Container className="container">
-      <Box 
+        <Box
           component="img"
           src="/home_pic.webp" // Replace with your image path
           alt="Home Page"
-          sx={{ width: '100%', height: 'auto', marginBottom: '24px' }} // Adjust width and height as needed
+          sx={{ width: "100%", height: "auto", marginBottom: "24px" }} // Adjust width and height as needed
         />
         <Grid container spacing={3} className="grid-container">
           <Grid item xs={12} sm={6} md={4} className="grid-item">
             <ItemCard
               image="/home_pic.webp"
               title="Sample Item 1"
-              link="/product/1" //add link to product here 
+              link="/product/1" //add link to product here
             />
           </Grid>
           <Grid item xs={12} sm={6} md={4} className="grid-item">
@@ -31,7 +31,7 @@ const Home = () => {
               title="Sample Item 2"
               description="This is another description of the item. It's just as cool!"
               price="$29.99"
-              link="/product/1" //add link to product here 
+              link="/product/2" //add link to product here
             />
           </Grid>
           <Grid item xs={12} sm={6} md={4} className="grid-item">
@@ -40,55 +40,54 @@ const Home = () => {
               title="Sample Item 3"
               description="This item is also pretty cool. You might want to consider buying it."
               price="39.99"
-              link="/product/1" //add link to product here 
+              link="/product/3" //add link to product here
             />
           </Grid>
           <Grid item xs={12} sm={6} md={4} className="grid-item">
             <ItemCard
               image="https://via.placeholder.com/345x200"
-              title="Sample Item 3"
+              title="Sample Item 4"
               description="This item is also pretty cool. You might want to consider buying it."
               price="39.99"
-              link="/product/1" //add link to product here 
+              link="/product/4" //add link to product here
             />
           </Grid>
           <Grid item xs={12} sm={6} md={4} className="grid-item">
             <ItemCard
               image="https://via.placeholder.com/345x200"
-              title="Sample Item 3"
+              title="Sample Item 5"
               description="This item is also pretty cool. You might want to consider buying it."
               price="39.99"
-              link="/product/1" //add link to product here 
+              link="/product/1" //add link to product here
             />
           </Grid>
           <Grid item xs={12} sm={6} md={4} className="grid-item">
             <ItemCard
               image="https://via.placeholder.com/345x200"
-              title="Sample Item 3"
+              title="Sample Item 6"
               description="This item is also pretty cool. You might want to consider buying it."
               price="39.99"
-              link="/product/1" //add link to product here 
+              link="/product/1" //add link to product here
             />
           </Grid>
           <Grid item xs={12} sm={6} md={4} className="grid-item">
             <ItemCard
               image="https://via.placeholder.com/345x200"
-              title="Sample Item 3"
+              title="Sample Item 7"
               description="This item is also pretty cool. You might want to consider buying it."
               price="39.99"
-              link="/product/1" //add link to product here 
+              link="/product/1" //add link to product here
             />
           </Grid>
           <Grid item xs={12} sm={6} md={4} className="grid-item">
             <ItemCard
               image="https://via.placeholder.com/345x200"
-              title="Sample Item 3"
+              title="Sample Item 8"
               description="This item is also pretty cool. You might want to consider buying it."
               price="39.99"
-              link="/product/1" //add link to product here 
+              link="/product/1" //add link to product here
             />
           </Grid>
-          
         </Grid>
       </Container>
     </div>
