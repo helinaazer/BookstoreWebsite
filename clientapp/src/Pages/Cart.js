57aaa97b8a00d74fd8ef4d91b7603f0cd5293324
Helina Azer <hazer@uw.edu>
2024-08-29 12:23:57 -0700

    updating navbar for user and admin

--- clientapp/src/Pages/Cart.js
+++ clientapp/src/Pages/Cart.js
@@ -1,35 +1,38 @@
-import React from 'react';
-import ItemCardBig from '../Components/ItemCardBig';
-import NavBar from '../Components/NavBar';
-import Button from '@mui/material/Button'; // Import the Button component from Material-UI
+import React from "react";
+import ItemCardBig from "../Components/ItemCardBig";
+import NavBar from "../Components/NavBar";
+import Button from "@mui/material/Button"; // Import the Button component from Material-UI
 import "./Cart.css";
+import { UserProvider } from "../Components/UserAdminContext";
 
 const Cart = () => {
   const cartItems = [
     {
       id: 1,
-      image: 'https://via.placeholder.com/150',
-      title: 'Christopher Knight Home Daisy Outdoor Club Chair with Cushion (Set of 4), Teak Finish, Cream',
-      description: '',
+      image: "https://via.placeholder.com/150",
+      title:
+        "Christopher Knight Home Daisy Outdoor Club Chair with Cushion (Set of 4), Teak Finish, Cream",
+      description: "",
       price: 824.91,
       quantity: 1,
       stockNumber: 8,
-      
     },
     {
       id: 2,
-      image: 'https://via.placeholder.com/150',
-      title: 'Christopher Knight Home Daisy Outdoor Club Chair with Cushion (Set of 4), Teak Finish, Cream',
-      description: '',
+      image: "https://via.placeholder.com/150",
+      title:
+        "Christopher Knight Home Daisy Outdoor Club Chair with Cushion (Set of 4), Teak Finish, Cream",
+      description: "",
       price: 824.91,
       quantity: 1,
       stockNumber: 8,
     },
     {
       id: 3,
-      image: 'https://via.placeholder.com/150',
-      title: 'Christopher Knight Home Daisy Outdoor Club Chair with Cushion (Set of 4), Teak Finish, Cream',
-      description: '',
+      image: "https://via.placeholder.com/150",
+      title:
+        "Christopher Knight Home Daisy Outdoor Club Chair with Cushion (Set of 4), Teak Finish, Cream",
+      description: "",
       price: 824.91,
       quantity: 1,
       stockNumber: 8,
@@ -37,20 +40,23 @@ const Cart = () => {
   ];
 
   const handleRemove = (id) => {
-    console.log('Remove item with id:', id);
+    console.log("Remove item with id:", id);
   };
 
   const handleProceedToOrder = () => {
-    console.log('Proceed to Order');
+    console.log("Proceed to Order");
     // Add functionality to handle the order process
   };
 
   return (
     <div>
-      <NavBar />
-      <div className="header">
-        Shopping Cart
-      </div>
+      <UserProvider>
+        <NavBar
+          logoSrc="/bookstoreLogo.jpg"
+          title="St. Mary's Coptic Orthodox Church Bookstore"
+        />
+      </UserProvider>
+      <div className="header">Shopping Cart</div>
       {cartItems.map((item) => (
         <div key={item.id}>
           <ItemCardBig
@@ -60,22 +66,31 @@ const Cart = () => {
             quantity={item.quantity}
             onRemove={() => handleRemove(item.id)}
             stockNumber={item.stockNumber}
-            isCart={true} 
+            isCart={true}
           />
         </div>
       ))}
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end', marginTop: '10px' }}>
-        <h2 style={{ marginBottom: '20px' }}>
-          Subtotal ({cartItems.length} item): ${cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0).toFixed(2)}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-end",
+          marginTop: "10px",
+        }}
+      >
+        <h2 style={{ marginBottom: "20px" }}>
+          Subtotal ({cartItems.length} item): $
+          {cartItems
+            .reduce((acc, item) => acc + item.price * item.quantity, 0)
+            .toFixed(2)}
         </h2>
         <Button
           variant="contained"
           onClick={handleProceedToOrder}
-          className='orderbutton' // Custom background and text color
+          className="orderbutton" // Custom background and text color
         >
           Proceed to Order
         </Button>
-
       </div>
     </div>
   );
